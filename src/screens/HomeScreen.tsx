ba77e3f3a94e528701a12705123e8ebae0035eae
Diomedse <diomedesswhite@gmail.com>
2026-02-21 11:03:19 -0500

    Add HomeScreen component for Lectoplex app

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+// HomeScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Welcome to Lectoplex!</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
